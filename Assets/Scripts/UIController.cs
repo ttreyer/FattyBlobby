ab4c2e1a0821041dba2320d4271c5cf8bdce6526
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2018-08-11 18:20:09 +0200

    Fix UIController, start Laser Prefabs

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -9,9 +9,9 @@ public class UIController : MonoBehaviour
 {
 	private float scaleTimer;
 	private float TimerBarWidth;
-	private Image UITimerBar;
-	private Image UIPlayer;
-	private Image UIPlayerNext;
+	private RectTransform UITimerBar;
+	private RectTransform UIPlayer;
+	private RectTransform UIPlayerNext;
 
 	public void Init(float scaleTimer, Vector3 current, Vector3 next)
 	{
@@ -22,21 +22,21 @@ public class UIController : MonoBehaviour
 	void Start ()
 	{
 		scaleTimer = 0;
-		UITimerBar   = GameObject.FindGameObjectWithTag("scaleTimerBar");
-		TimerBarWidth = UITimerBar.preferredWidth;
-		UIPlayer     = GameObject.FindGameObjectWithTag("scalePlayer");
-		UIPlayerNext = GameObject.FindGameObjectWithTag("scalePlayerNext");
+		UITimerBar   = GameObject.FindGameObjectWithTag("scaleTimerBar").GetComponent<RectTransform>();
+		TimerBarWidth = UITimerBar.rect.width;
+		UIPlayer     = GameObject.FindGameObjectWithTag("scalePlayer").GetComponent<RectTransform>();
+		UIPlayerNext = GameObject.FindGameObjectWithTag("scalePlayerNext").GetComponent<RectTransform>();
 	}
 
 	public void UpdateScaleTimer(float scaleTime)
 	{
-		UITimerBar.rectTransform.sizeDelta = new Vector2(TimerBarWidth * ((scaleTimer - scaleTime) * 100 / scaleTimer), UITimerBar.preferredHeight);
+		UITimerBar.sizeDelta = new Vector2(TimerBarWidth * ((scaleTimer - scaleTime) * 100 / scaleTimer), UITimerBar.rect.height);
 	}
 
 	void UpdateUIPlayer(Vector3 current, Vector3 next)
 	{
-		UIPlayer.rectTransform.sizeDelta = current;
-		UIPlayerNext.rectTransform.sizeDelta = next;
+		UIPlayer.sizeDelta = current;
+		UIPlayerNext.sizeDelta = next;
 	}
 
 	public void UpdatePlayerSize(Vector3 current, Vector3 next)
