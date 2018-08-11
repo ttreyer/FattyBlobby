738ee960fe3ab01ad193b7c0103c090fed4b6ef0
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2018-08-11 17:55:12 +0200

    Add UIController

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,21 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Timers;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIController : MonoBehaviour {
+public class UIController : MonoBehaviour
+{
+	private float scaleTimer;
+	private float TimerBarWidth;
+	private Image UITimerBar;
+	private Image UIPlayer;
+	private Image UIPlayerNext;
 
-	public void Init()
+	public void Init(float scaleTimer, Vector3 current, Vector3 next)
 	{
-		
+		this.scaleTimer = scaleTimer;
+		UpdateUIPlayer(current, next);
 	}
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		scaleTimer = 0;
+		UITimerBar   = GameObject.FindGameObjectWithTag("scaleTimerBar");
+		TimerBarWidth = UITimerBar.preferredWidth;
+		UIPlayer     = GameObject.FindGameObjectWithTag("scalePlayer");
+		UIPlayerNext = GameObject.FindGameObjectWithTag("scalePlayerNext");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void UpdateScaleTimer(float scaleTime)
+	{
+		UITimerBar.rectTransform.sizeDelta = new Vector2(TimerBarWidth * ((scaleTimer - scaleTime) * 100 / scaleTimer), UITimerBar.preferredHeight);
+	}
+
+	void UpdateUIPlayer(Vector3 current, Vector3 next)
+	{
+		UIPlayer.rectTransform.sizeDelta = current;
+		UIPlayerNext.rectTransform.sizeDelta = next;
+	}
+
+	public void UpdatePlayerSize(Vector3 current, Vector3 next)
+	{
+		UpdateUIPlayer(current, next);
 	}
 }
