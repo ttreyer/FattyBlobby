8c87a30f079f1fc50d86abdddfb1b6410dbc2f37
Yvan Pannatier <pannatieryvan94@gmail.com>
2018-08-11 17:41:48 +0200

    Adding Time and growing mecanics

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public float scaleTime;
+    public Vector3 initialPlayerScale;
+    private float scaleTimer;
+    private GameObject player;
+    private PlayerController playerController;
+    private GameObject ui;
+    private UIController uiController;
+
+
+    private void Start () {
+
+        initScaleTimer();
+        //Get player and  controller
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<PlayerController>();
+
+        //Get UI and controller
+        ui = GameObject.FindGameObjectWithTag("Canvas");
+        uiController = ui.GetComponent<UIController>();
+
+    
+        playerController.Init(initialPlayerScale);
+        uiController.Init();
+
+    }
+
+    private void LateUpdate () {
+        scaleTimer -= Time.deltaTime;
+
+        if(scaleTimer <= 0)
+        {
+            playerController.Grow();
+            initScaleTimer();
+        }
+	}
+
+    private void initScaleTimer()
+    {
+        scaleTimer = scaleTime;
+    }
+}
